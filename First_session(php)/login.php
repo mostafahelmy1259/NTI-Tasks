46ea481d8a5723621d4d98607c74739cdcb20b3a
mostafahelmy <115579976+mostafahelmy1259@users.noreply.github.com>
2025-07-14 12:17:49 +0300

    modified

--- First_session(php)/login.php
+++ First_session(php)/login.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
   <meta charset="UTF-8">
-  <title>Day 1 ==> Task</title>
+  <title>Day 1</title>
   <link href="css/bootstrap.css" rel="stylesheet" >
 </head>
 <body class="bg-success">
